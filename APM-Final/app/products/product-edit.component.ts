51ca0eae765c7379faf57e7dd58d9975551e58d5
Deborah Kurata <deborahk@insteptech.com>
2016-12-29 13:07:21 -0800

    First Checkin

---
+++ APM-Final/app/products/product-edit.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router  } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/merge';
+import { Subscription } from 'rxjs/Subscription';
+
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+    templateUrl: 'app/products/product-edit.component.html'
+})
+export class ProductEditComponent implements OnInit, OnDestroy {
+    @ViewChild(NgForm) productForm: NgForm;
+
+    pageTitle: string = 'Product Edit';
+    errorMessage: string;
+
+    product: IProduct;
+    private sub: Subscription;
+
+    constructor(private route: ActivatedRoute,
+                private router: Router,
+                private productService: ProductService) { }
+
+    ngOnInit(): void {
+        // Read the product Id from the route parameter
+        this.sub = this.route.params.subscribe(
+            params => {
+                let id = +params['id'];
+                this.getProduct(id);
+            }
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
+
+    getProduct(id: number): void {
+        this.productService.getProduct(id)
+            .subscribe(
+                (product: IProduct) => this.onProductRetrieved(product),
+                (error: any) => this.errorMessage = <any>error
+            );
+    }
+
+    onProductRetrieved(product: IProduct): void {
+        if (this.productForm) {
+            this.productForm.reset();
+        }
+        this.product = product;
+
+        if (this.product.id === 0) {
+            this.pageTitle = 'Add Product';
+        } else {
+            this.pageTitle = `Edit Product: ${this.product.productName}`;
+        }
+    }
+
+    deleteProduct(): void {
+        if (this.product.id === 0) {
+            // Don't delete, it was never saved.
+            this.onSaveComplete();
+       } else {
+            if (confirm(`Really delete the product: ${this.product.productName}?`)) {
+                this.productService.deleteProduct(this.product.id)
+                    .subscribe(
+                        () => this.onSaveComplete(),
+                        (error: any) => this.errorMessage = <any>error
+                    );
+            }
+        }
+    }
+
+    saveProduct(): void {
+        if (this.productForm.dirty && this.productForm.valid) {
+            this.productService.saveProduct(this.product)
+                .subscribe(
+                    () => this.onSaveComplete(),
+                    (error: any) => this.errorMessage = <any>error
+                );
+        } else if (!this.productForm.dirty) {
+            this.onSaveComplete();
+        }
+    }
+
+    onSaveComplete(): void {
+        // Reset the form to clear the flags
+        this.productForm.reset();
+        this.router.navigate(['/products']);
+    }
+}
---
+++ APM-Final/readme.md
@@ -0,0 +1,25 @@
+
+## Description
+
+The edit form in this reference application demonstrates: 
+ - FormControls, FormGroups, FormArrays
+ - Basic validation
+ - Custom validation
+ - Generic validation (including blur)
+ - Display of validation messages
+ - Dynamically adding input elements
+ - Passing route parameters
+ - Route guards (including confirmation when navigating away with unsaved changes)
+ - Populating the form data from Http using Observables
+ - Basic CRUD operations: Create, Read, Update, and Delete.
+
+## Installation
+
+1) Open a command prompt in the project's root directory (APM)
+
+2) Type: `npm install`
+    This installs the dependencies as defined in the package.json file.
+    
+3) Type: `npm start`
+    This launches the TypeScript compiler (tsc) to compile the application and wait for changes. 
+    It also starts the lite-server and launches the browser to run the application.
