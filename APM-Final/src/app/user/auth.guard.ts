32b805744765d8f48b21a0d55d6d0260c92e541e
Deborah Kurata <deborahk@insteptech.com>
2019-09-02 14:59:12 -0700

    Update canLoad to access full route (w/parameters)

--- APM-Final/src/app/user/auth.guard.ts
+++ APM-Final/src/app/user/auth.guard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, Route } from '@angular/router';
+import { CanActivate, CanLoad, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, Route, UrlSegment } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate, CanLoad {
 
@@ -17,8 +17,10 @@ export class AuthGuard implements CanActivate, CanLoad {
     return this.checkLoggedIn(state.url);
   }
 
-  canLoad(route: Route): boolean {
-    return this.checkLoggedIn(route.path);
+  // Use the segments to build the full route
+  // when using canLoad
+  canLoad(route: Route, segments: UrlSegment[]): boolean {
+    return this.checkLoggedIn(segments.join('/'));
   }
 
   checkLoggedIn(url: string): boolean {
